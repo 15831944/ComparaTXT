0dc4c96e682f2e93320111c8dc020b3e4734912a
Lino Cesar Cutini <cesarcutini@gmail.com>
2018-08-28 14:48:41 -0300

    Add files via upload

---
+++ ComparaTXT/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ComparaTXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        	this.button2 = new System.Windows.Forms.Button();
+        	this.button3 = new System.Windows.Forms.Button();
+        	this.button4 = new System.Windows.Forms.Button();
+        	this.textBox2 = new System.Windows.Forms.TextBox();
+        	this.listBox1 = new System.Windows.Forms.ListBox();
+        	this.button1 = new System.Windows.Forms.Button();
+        	this.button5 = new System.Windows.Forms.Button();
+        	this.button6 = new System.Windows.Forms.Button();
+        	this.SuspendLayout();
+        	// 
+        	// button2
+        	// 
+        	this.button2.Location = new System.Drawing.Point(14, 12);
+        	this.button2.Name = "button2";
+        	this.button2.Size = new System.Drawing.Size(104, 23);
+        	this.button2.TabIndex = 1;
+        	this.button2.Text = "Arquivo Sistema";
+        	this.button2.UseVisualStyleBackColor = true;
+        	this.button2.Click += new System.EventHandler(this.button2_Click);
+        	// 
+        	// button3
+        	// 
+        	this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+        	this.button3.Location = new System.Drawing.Point(535, 555);
+        	this.button3.Name = "button3";
+        	this.button3.Size = new System.Drawing.Size(75, 23);
+        	this.button3.TabIndex = 4;
+        	this.button3.Text = "Comparar";
+        	this.button3.UseVisualStyleBackColor = true;
+        	this.button3.Click += new System.EventHandler(this.button3_Click);
+        	// 
+        	// button4
+        	// 
+        	this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+        	this.button4.Location = new System.Drawing.Point(454, 555);
+        	this.button4.Name = "button4";
+        	this.button4.Size = new System.Drawing.Size(75, 23);
+        	this.button4.TabIndex = 5;
+        	this.button4.Text = "Cancelar";
+        	this.button4.UseVisualStyleBackColor = true;
+        	this.button4.Click += new System.EventHandler(this.button4_Click);
+        	// 
+        	// textBox2
+        	// 
+        	this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+        	this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+        	this.textBox2.Location = new System.Drawing.Point(124, 13);
+        	this.textBox2.Name = "textBox2";
+        	this.textBox2.Size = new System.Drawing.Size(485, 20);
+        	this.textBox2.TabIndex = 8;
+        	// 
+        	// listBox1
+        	// 
+        	this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+        	this.listBox1.FormattingEnabled = true;
+        	this.listBox1.Location = new System.Drawing.Point(13, 48);
+        	this.listBox1.Name = "listBox1";
+        	this.listBox1.Size = new System.Drawing.Size(596, 498);
+        	this.listBox1.TabIndex = 9;
+        	// 
+        	// button1
+        	// 
+        	this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+        	this.button1.Location = new System.Drawing.Point(13, 555);
+        	this.button1.Name = "button1";
+        	this.button1.Size = new System.Drawing.Size(75, 23);
+        	this.button1.TabIndex = 10;
+        	this.button1.Text = "Imprimir";
+        	this.button1.UseVisualStyleBackColor = true;
+        	this.button1.Click += new System.EventHandler(this.Button1Click);
+        	// 
+        	// button5
+        	// 
+        	this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+        	this.button5.Location = new System.Drawing.Point(94, 555);
+        	this.button5.Name = "button5";
+        	this.button5.Size = new System.Drawing.Size(75, 23);
+        	this.button5.TabIndex = 11;
+        	this.button5.Text = "Salvar LOG";
+        	this.button5.UseVisualStyleBackColor = true;
+        	this.button5.Click += new System.EventHandler(this.Button5Click);
+        	// 
+        	// button6
+        	// 
+        	this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+        	this.button6.Location = new System.Drawing.Point(175, 555);
+        	this.button6.Name = "button6";
+        	this.button6.Size = new System.Drawing.Size(75, 23);
+        	this.button6.TabIndex = 12;
+        	this.button6.Text = "Abrir LOG";
+        	this.button6.UseVisualStyleBackColor = true;
+        	this.button6.Click += new System.EventHandler(this.Button6Click);
+        	// 
+        	// Form1
+        	// 
+        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.ClientSize = new System.Drawing.Size(619, 587);
+        	this.Controls.Add(this.button6);
+        	this.Controls.Add(this.button5);
+        	this.Controls.Add(this.button1);
+        	this.Controls.Add(this.listBox1);
+        	this.Controls.Add(this.textBox2);
+        	this.Controls.Add(this.button4);
+        	this.Controls.Add(this.button3);
+        	this.Controls.Add(this.button2);
+        	this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+        	this.Name = "Form1";
+        	this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        	this.Text = "Comparar TXT";
+        	this.ResumeLayout(false);
+        	this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ComparaTXT/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.Collections.Generic;
+
+namespace ComparaTXT
+{
+	public partial class Form1 : Form
+	{
+		string[] arquivoSistema, arquivoRede;
+		static string arquivoSistemaName, arquivoRedeName;
+		
+		private PrintDocument document = new PrintDocument();
+
+		public Dictionary<int, string> dicio = new Dictionary<int, string>()
+		{
+			{ 0, "sigla" }, { 1, "parte" }, { 2, "codigo" }, { 4, "posição" },
+			{ 5, "quantidade" }, { 6, "largura" }, { 7, "comprimento" },
+			{ 8, "chanfro" }, { 9, "quant. furos" }, { 10, "recortes" }, { 11, "dobra" },
+			{ 12, "abertura de aba" }, { 13, "furos esp." }, { 14, "quant. diametros" },
+			{ 15, "trusq. min." }, { 16, "solda" }
+		};
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		
+		private static void run_cmd()
+		{
+			File.Delete(@"C:\Temp\caminho.txt");
+
+			if (File.Exists(@"M:\Projetos\PROGRAMAS\Ler CAM\LerCAM.exe"))
+			{
+				Process cmd = new Process();
+				cmd.StartInfo.FileName = @"M:\Projetos\PROGRAMAS\Ler CAM\LerCAM.exe";
+				cmd.StartInfo.RedirectStandardInput = true;
+				cmd.StartInfo.RedirectStandardOutput = false;
+				cmd.StartInfo.CreateNoWindow = false;
+				cmd.StartInfo.UseShellExecute = false;
+				cmd.Start();
+	
+				cmd.StandardInput.Flush();
+				cmd.StandardInput.Close();
+				cmd.WaitForExit();
+			}
+			else
+			{
+				MessageBox.Show("Não foi possível abrir o programa \"Ler CAM\", verifique na pasta!");
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			run_cmd();
+
+			arquivoSistemaName = File.ReadAllText(@"C:\Temp\caminho.txt");
+			
+			textBox2.Text = arquivoSistemaName;
+			
+			try
+			{
+				arquivoSistema = File.ReadAllLines(arquivoSistemaName);
+
+				string path = Path.GetDirectoryName(arquivoSistemaName);
+				
+				DirectoryInfo files = new DirectoryInfo(path);
+				FileInfo[] arquivos = files.GetFiles();
+				Array.Sort(arquivos, (FileInfo a, FileInfo b) => DateTime.Compare(a.CreationTime, b.CreationTime));
+				Array.Reverse(arquivos);
+				
+				var query = from a in arquivos
+				            where a.Name.Contains("OPER")
+				            select a;
+
+				arquivoRedeName = query.First().ToString();
+			}
+			catch
+			{
+				MessageBox.Show("O LerCAM foi interrompido ou deu erro na execução!\n" + "Verifique e tente novamente.");
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (textBox2.Text != string.Empty)
+			{
+				listBox1.Items.Clear();
+				
+				AnalisaTXT();
+			}
+			else
+				MessageBox.Show("Por favor, clique em 'Arquivo Sistema'!");
+		}
+        
+		private void AnalisaTXT()
+		{
+			try
+			{
+				string path = Path.GetDirectoryName(arquivoSistemaName);
+				path += (char)92;
+				arquivoRede = File.ReadAllLines(path + arquivoRedeName);
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Não foi possível ler o arquivo \"OPER\". Favor verificar!");
+			}
+
+			string position = string.Empty;
+
+			int a = 0;
+			foreach (string linhaArquivoRede in arquivoRede)
+			{
+				string[] arrayLinhaArquivoRede = linhaArquivoRede.Split(';');
+
+				foreach (string linhaArquivoSistema in arquivoSistema)
+				{
+					string[] arrayLinhaArquivoSistema = linhaArquivoSistema.Split(';');
+
+					if (arrayLinhaArquivoSistema[4] == arrayLinhaArquivoRede[4])
+					{
+						int i = 0;
+
+						foreach (string indexArrayLinhaArquivoSistema in arrayLinhaArquivoSistema)
+						{
+							string indexArrayLinhaArquivoRede = arrayLinhaArquivoRede[i];
+							position = arrayLinhaArquivoRede[4];
+							
+							string indexArrayLinhaSistema = string.Empty;
+
+							if (indexArrayLinhaArquivoRede == "0")
+								indexArrayLinhaArquivoRede = "";
+							
+							if (indexArrayLinhaArquivoSistema == "0")
+								indexArrayLinhaSistema = "";
+							else
+								indexArrayLinhaSistema = indexArrayLinhaArquivoSistema;
+
+							if (i == 15)
+								indexArrayLinhaArquivoRede = indexArrayLinhaArquivoRede.Replace(".0", "");
+
+							if (indexArrayLinhaSistema != indexArrayLinhaArquivoRede) //(indexArrayLinhaArquivoSistema != indexArrayLinhaArquivoRede)
+							{
+								switch (i)
+								{
+									case 4:
+									case 6:
+									case 7:
+									case 8:
+									case 9:
+									case 10:
+									case 11:
+									case 12:
+									case 13:
+									case 14:
+									case 15:
+									case 16:
+										if (!(listBox1.Items.Contains("Verificar " + dicio[i] + " da posição -> " + position)))
+											listBox1.Items.Add("Verificar " + dicio[i] + " da posição -> " + position);
+										break;
+								}
+							}
+							i += 1;
+						}
+					}
+				}
+				a += 1;
+			}
+			
+			Resultado();
+		}
+        
+		private void Resultado()
+		{
+			if (listBox1.Items.Count == 0 && arquivoRede != null)
+				listBox1.Items.Add("Não há diferenças");
+		}
+
+		private void Button1Click(object sender, EventArgs e)
+		{
+			PrintPreviewDialog ppd = new PrintPreviewDialog();
+			ppd.Document = document;
+			ppd.Document.DocumentName = "Log";
+			document.PrintPage += document_PrintPage;
+			ppd.ShowDialog();
+		}
+		
+		private void document_PrintPage(object sender, PrintPageEventArgs e)
+		{
+			e.Graphics.PageUnit = GraphicsUnit.Millimeter;
+			const int leading = 2;
+			const int leftMargin = 10;
+			const int topMargin = 5;
+	
+			// a few simple formatting options..
+			StringFormat FmtRight = new StringFormat() { Alignment = StringAlignment.Near };
+			StringFormat FmtLeft = new StringFormat() { Alignment = StringAlignment.Near };
+			StringFormat FmtCenter = new StringFormat() { Alignment = StringAlignment.Near };
+	
+			StringFormat fmt = FmtRight;
+	
+			using (Font font = new Font("Arial Narrow", 10f))
+			{
+				SizeF sz = e.Graphics.MeasureString("_|", Font);
+				float h = sz.Height + leading;
+	
+				for (int i = 0; i < listBox1.Items.Count; i++)
+					e.Graphics.DrawString(listBox1.Items[i].ToString(), font, Brushes.Black, 
+						leftMargin, topMargin + h * i, fmt);
+			}
+		}
+
+		void Button5Click(object sender, EventArgs e)
+		{
+			string caminho = File.ReadAllText(@"C:\Temp\caminho.txt");
+			string path = Path.GetDirectoryName(caminho);
+			string data = (DateTime.Now.ToString()).Replace("/", "-").Replace(":", "-");
+			string inicio = Path.GetFileNameWithoutExtension(arquivoSistemaName);
+			string arquivo = (path + (char)92 + inicio + "_LogCompara_" + data + ".txt");
+
+			string[] logs = new string[listBox1.Items.Count];
+
+			for (int i = 0; i < listBox1.Items.Count; i++)
+			{
+				logs[i] = listBox1.GetItemText(listBox1.Items[i]);
+			}
+
+			File.WriteAllLines(arquivo, logs);
+		}
+
+		void Button6Click(object sender, EventArgs e)
+		{
+			try
+			{
+				string caminho = File.ReadAllText(@"C:\Temp\caminho.txt");
+				string path = Path.GetDirectoryName(caminho);
+
+				DirectoryInfo files = new DirectoryInfo(path);
+				FileInfo[] arquivos = files.GetFiles();
+				Array.Sort(arquivos, (FileInfo a, FileInfo b) => DateTime.Compare(a.CreationTime, b.CreationTime));
+				Array.Reverse(arquivos);
+
+				var query = from a in arquivos
+				            where a.Name.Contains("_LOG_")
+				            select a;
+
+				Process.Start("notepad.exe",path + (char)92 + query.First());
+			}
+			catch
+			{
+				MessageBox.Show("O LogCompara não foi encontrado!\n" + "Verifique e tente novamente.");
+			}
+		}
+	}
+}
